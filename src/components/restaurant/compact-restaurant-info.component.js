e31315f4b0b7c571f51db858a818c52f76dbeaca
BrodyMouw <BrodyMouw32@live.com>
2024-08-23 15:01:13 -0700

    working callout and marker

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,17 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import WebView from "react-native-webview";
 import { Platform } from "react-native";
 
 import { Text } from "../typography/text.component";
 
 const CompactImage = styled.Image`
-  border-radius: 10px;
-  width: 120px;
-  height: 100px;
-`;
-
-const CompactWebview = styled(WebView)`
   border-radius: 10px;
   width: 120px;
   height: 100px;
@@ -23,10 +16,8 @@ const Item = styled.View`
   align-items: center;
 `;
 
-const isAndroid = Platform.OS === "android";
-
 export const CompactRestaurantInfo = ({ restaurant }) => {
-  const Image = isAndroid ? CompactWebview : CompactImage;
+  const Image = CompactImage;
 
   return (
     <Item>
--- src/infrastructure/theme/colors.js
+++ src/infrastructure/theme/colors.js
@@ -17,4 +17,12 @@ export const colors = {
     primary: "#FFFFFF",
     secondary: "#F1F1F1",
   },
+  text: {
+    primary: "#262626",
+    secondary: "#757575",
+    disabled: "#9C9C9C",
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
 };
