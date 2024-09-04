2d254a994d2ef3b65e5bd857d3910aa6cc276a9b
BrodyMouw <BrodyMouw32@live.com>
2024-09-03 19:48:20 -0700

    account and login

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,5 +1,5 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.js";
 
-const loginRequest = (email, password) =>
+export const loginRequest = (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
