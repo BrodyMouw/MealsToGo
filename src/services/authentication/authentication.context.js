1d873c24edff2d2b37b7ad2b9ad8b01382c1552f
BrodyMouw <BrodyMouw32@live.com>
2024-09-18 18:40:20 -0700

    login and auth

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,6 +1,8 @@
 import React, { useState, createContext } from "react";
 
-import { loginRequest } from "./authentication.service";
+import { loginRequest, register, onSignOut } from "./authentication.service";
+import { auth } from "../../firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AuthenticationContext = createContext();
 
@@ -8,6 +10,15 @@ export const AuthenticationContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+
+  onAuthStateChanged(auth, (usr) => {
+    if (usr) {
+      setUser(usr);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  });
 
   const onLogin = (email, password) => {
     setIsLoading(true);
@@ -18,8 +29,32 @@ export const AuthenticationContextProvider = ({ children }) => {
       })
       .catch((e) => {
         setIsLoading(false);
-        setError(e);
+        setError(e.toString());
       });
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    setIsLoading(true);
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    register(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
+  const onLogout = () => {
+    onSignOut().then(() => {
+      setUser(null);
+      setError(null);
+    });
   };
 
   return (
@@ -30,6 +65,8 @@ export const AuthenticationContextProvider = ({ children }) => {
         isLoading,
         error,
         onLogin,
+        onRegister,
+        onLogout,
       }}
     >
       {children}
--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,5 +1,14 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../../firebase.js";
 
 export const loginRequest = (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
+
+export const register = (email, password) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+export const onSignOut = () => signOut(auth);
